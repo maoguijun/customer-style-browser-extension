7bbbf5805432b976df3965d008f9281bb0a59c28
maoguijun <maoguijun@mini1.cn>
2022-04-08 10:45:21 +0800

    添加多删掉的问题

--- popup/editorCus.js
+++ popup/editorCus.js
@@ -2,19 +2,22 @@
  * @Author: maoguijun
  * @Date: 2022-04-07 20:07:32
  * @LastEditors: maoguijun
- * @LastEditTime: 2022-04-07 21:03:04
- * @FilePath: \customer-style-browser-extension\editorCus.js
+ * @LastEditTime: 2022-04-08 10:44:04
+ * @FilePath: \customer-style-browser-extension\popup\editorCus.js
  */
 
-const editor = ace.edit("customStyle");
+try {
+  const editor = ace.edit("customStyle");
+  editor.session.setMode("ace/mode/css");
+  editor.setFontSize(18);
+  editor.setHighlightActiveLine(true);
+  editor.on("change", (e) => {
+    editerChange(editor);
+  });
 
-editor.session.setMode("ace/mode/css");
-editor.on("change", (e) => {
-  editerChange(editor);
-});
-
-// 弹出输入框的时候，自动填充样式代码
-chrome.storage.sync.get("customStyle", ({ customStyle }) => {
-  // editor.value = customStyle;
-  editor.setValue(customStyle);
-});
+  // 弹出输入框的时候，自动填充样式代码
+  chrome.storage.sync.get("customStyle", ({ customStyle }) => {
+    // editor.value = customStyle;
+    editor.setValue(customStyle);
+  });
+} catch (error) {}
