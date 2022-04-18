e2c28843b8b67569d30d78707824ee7d066285b5
zaric <xuat@qq.com>
2022-04-19 01:21:36 +0800

    feat:显示css属性开发（差scrollto）

--- options/contextMenu.js
+++ options/contextMenu.js
@@ -1,15 +1,18 @@
+// import cssProperties from './assets/cssproperties';
 let page = document.getElementById("buttonDiv");
-page.addEventListener("click", handleButtonClick);
-const color = 'AliceBlue|AntiqueWhite|Aqua|Aquamarine|Azure|Beige|Bisque|Black|BlanchedAlmond|Blue|BlueViolet|Brown|BurlyWood|CadetBlue|Chartreuse|Chocolate|Coral|CornflowerBlue|Cornsilk|Crimson|Cyan|DarkBlue|DarkCyan|DarkGoldenRod|DarkGray|DarkGreen|DarkKhaki|DarkMagenta|DarkOliveGreen|DarkOrange|DarkOrchid|DarkRed|DarkSalmon|DarkSeaGreen|DarkSlateBlue|DarkSlateGray|DarkTurquoise|DarkViolet|DeepPink|DeepSkyBlue|DimGray|DodgerBlue|FireBrick|FloralWhite|ForestGreen|Fuchsia|Gainsboro|GhostWhite|Gold|GoldenRod|Gray|Green|GreenYellow|HoneyDew|HotPink|IndianRed |Indigo  |Ivory|Khaki|Lavender|LavenderBlush|LawnGreen|LemonChiffon|LightBlue|LightCoral|LightCyan|LightGoldenRodYellow|LightGray|LightGreen|LightPink|LightSalmon|LightSeaGreen|LightSkyBlue|LightSlateGray|LightSteelBlue|LightYellow|Lime|LimeGreen|Linen|Magenta|Maroon|MediumAquaMarine|MediumBlue|MediumOrchid|MediumPurple|MediumSeaGreen|MediumSlateBlue|MediumSpringGreen|MediumTurquoise|MediumVioletRed|MidnightBlue|MintCream|MistyRose|Moccasin|NavajoWhite|Navy|OldLace|Olive|OliveDrab|Orange|OrangeRed|Orchid|PaleGoldenRod|PaleGreen|PaleTurquoise|PaleVioletRed|PapayaWhip|PeachPuff|Peru|Pink|Plum|PowderBlue|Purple|Red|RosyBrown|RoyalBlue|SaddleBrown|Salmon|SandyBrown|SeaGreen|SeaShell|Sienna|Silver|SkyBlue|SlateBlue|SlateGray|Snow|SpringGreen|SteelBlue|Tan|Teal|Thistle|Tomato|Turquoise|Violet|Wheat|White|WhiteSmoke|Yellow|YellowGreen';
-const length = '0%|0ch|0cm|0em|0ex|0fr|0in|0mm|0px|0pt|0px|0rem|0vh|0vw|0vmin|0vmax';
+// page.addEventListener("click", handleButtonClick);
+const inputDom = document.querySelector('.input');
+inputDom.addEventListener("change", (e) => { getRecommandResult(e.target.value) })
+const color = 'AliceBlue|AntiqueWhite|Aqua|Aquamarine|Azure|Beige|Bisque|Black|BlanchedAlmond|Blue|BlueViolet|Brown|BurlyWood|CadetBlue|Chartreuse|Chocolate|Coral|CornflowerBlue|Cornsilk|Crimson|Cyan|DarkBlue|DarkCyan|DarkGoldenRod|DarkGray|DarkGreen|DarkKhaki|DarkMagenta|DarkOliveGreen|DarkOrange|DarkOrchid|DarkRed|DarkSalmon|DarkSeaGreen|DarkSlateBlue|DarkSlateGray|DarkTurquoise|DarkViolet|DeepPink|DeepSkyBlue|DimGray|DodgerBlue|FireBrick|FloralWhite|ForestGreen|Fuchsia|Gainsboro|GhostWhite|Gold|GoldenRod|Gray|Green|GreenYellow|HoneyDew|HotPink|IndianRed |Indigo  |Ivory|Khaki|Lavender|LavenderBlush|LawnGreen|LemonChiffon|LightBlue|LightCoral|LightCyan|LightGoldenRodYellow|LightGray|LightGreen|LightPink|LightSalmon|LightSeaGreen|LightSkyBlue|LightSlateGray|LightSteelBlue|LightYellow|Lime|LimeGreen|Linen|Magenta|Maroon|MediumAquaMarine|MediumBlue|MediumOrchid|MediumPurple|MediumSeaGreen|MediumSlateBlue|MediumSpringGreen|MediumTurquoise|MediumVioletRed|MidnightBlue|MintCream|MistyRose|Moccasin|NavajoWhite|Navy|OldLace|Olive|OliveDrab|Orange|OrangeRed|Orchid|PaleGoldenRod|PaleGreen|PaleTurquoise|PaleVioletRed|PapayaWhip|PeachPuff|Peru|Pink|Plum|PowderBlue|Purple|Red|RosyBrown|RoyalBlue|SaddleBrown|Salmon|SandyBrown|SeaGreen|SeaShell|Sienna|Silver|SkyBlue|SlateBlue|SlateGray|Snow|SpringGreen|SteelBlue|Tan|Teal|Thistle|Tomato|Turquoise|Violet|Wheat|White|WhiteSmoke|Yellow|YellowGreen'.split('|');
+const length = '0%|0ch|0cm|0em|0ex|0fr|0in|0mm|0px|0pt|0px|0rem|0vh|0vw|0vmin|0vmax'.split('|');
 /**
  * TODOs：
  * 1. 添加滚动定位功能 保证选中的item永远在视野范围内
  * 2. 获取当前行中的输入，动态判断应该匹配的是csskey还是value，value匹配时忽略前方空格，仅从冒号前匹配\w，
  *    最好能获取左右键和点击的位置光标前方，不然推荐值可能有误（没法处理就移动或者点击后直接关掉context menu）
  * down----- 3. 菜单点击其他区域自动关闭，hover时动态添加active，点中后取值----down;
- * 4. 输入时的模糊搜索，比如输入wi 匹配 width ..... white-space 按照 wi_,w_i,_w_i_来推荐，value同理,
- *    最后用一个set去存要显示的推荐内容，避免重复
+ * down----- 4. 输入时的模糊搜索，比如输入wi 匹配 width ..... white-space 按照 wi_,w_i,_w_i_来推荐，value同理,
+ *    最后用一个set去存要显示的推荐内容，避免重复----down
  * 5. 菜单显示位置跟随输入时光标位置
  * 6. 跟随窗口大小适配菜单栏大小，最小不小于12px的字体，如果窗口太小装不下，用一个confirm提示一次，
  *    是否再次显示存在chrome.storage中，并在插件右上角显示感叹号可以再次打开这个confirm
@@ -71,9 +74,10 @@ function clickListener(e) {
 }
 function addClickListener() {
     // 点击其他区域消除菜单
-    document.addEventListener('click',(e) => {
+    document.addEventListener('click', (e) => {
         if (e.path[2].id !== 'contextMenuContainer' && e.target.id !== 'buttonDiv') {
-            document.getElementById("contextMenuContainer").style.display = 'none';
+            // document.getElementById("contextMenuContainer").style.display = 'none';
+            console.log('close menu')
         }
     });
     document.querySelectorAll('#contextMenuContainer > div').forEach(item => {
@@ -92,24 +96,23 @@ function removeKeyDownListener() {
     window.removeEventListener('keydown', keyLinstener);
 }
 
-function handleButtonClick() {
+function showContextMenu(recommandList) {
+    let count = 0;
     const contextMenuElement = document.getElementById("contextMenuContainer");
     if (!contextMenuElement) {
         const contextMenu = document.createElement("div");
         contextMenu.id = 'contextMenuContainer';
-        const item1 = document.createElement("div");
-        item1.className = 'active';
-        item1.innerText = 'item 1 \'s content';
-        const item2 = document.createElement("div");
-        item2.innerText = 'item 2 \'s content';
-        const item3 = document.createElement("div");
-        item3.innerText = 'item 3 \'s content';
-        const item4 = document.createElement("div");
-        item4.innerText = 'item 4 \'s content';
-        contextMenu.appendChild(item1);
-        contextMenu.appendChild(item2);
-        contextMenu.appendChild(item3);
-        contextMenu.appendChild(item4);
+        contextMenu.style['overflowY'] = 'scroll';
+
+        recommandList.forEach((item)=>{
+            const itemDom = document.createElement("div");
+            if(count === 0){
+                itemDom.className = 'active';
+            }
+            itemDom.innerText = item;
+            contextMenu.appendChild(itemDom);
+            count ++;
+        })
         document.body.appendChild(contextMenu)
         addKeyDownListener();
         addClickListener();
@@ -125,3 +128,38 @@ function handleButtonClick() {
     }
 }
 
+function getRecommandResult(value) {
+    let a = value; //用户输入的字符串
+    let str_exact = ['^' + a, ''].join('.*'); //转化成正则格式的字符串
+    let str_mid = '^' + [...a, ''].join('.*'); //转化成正则格式的字符串
+    let str_casual = ['', ...a, ''].join('.*'); //转化成正则格式的字符串
+    const temp = [];
+    const temp1 = [];
+    const temp2 = [];
+    let reg_exact = new RegExp(str_exact);
+    let reg_mid = new RegExp(str_mid);
+    let reg_casual = new RegExp(str_casual);
+    for (let key in cssProperties) {
+        if (reg_exact.test(key)) {
+            if (!temp.includes(key)) {
+                // console.log('%c 111', 'background:rgba(250,50,50,0.5)', key, cssProperties[key], str_exact);
+                temp.push(key)
+            }
+        }
+        if (reg_mid.test(key)) {
+            if (!temp.includes(key)) {
+                // console.log('%c 222', 'background:rgba(50,250,50,0.5)', key, cssProperties[key], str_mid);
+                temp1.push(key)
+            }
+        }
+        if (reg_casual.test(key)) {
+            if (!temp1.includes(key)) {
+                // console.log('%c 333', 'background:rgba(50,50,250,0.5)', key, cssProperties[key], str_casual);
+                temp2.push(key)
+            }
+        }
+    }
+    let recommandRes = new Set(temp.concat(temp1).concat(temp2));
+    console.log('res============', recommandRes);
+    showContextMenu(recommandRes);
+}
